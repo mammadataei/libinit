a2399442d61c1ff203639c5625cdb7f4841cca35
Mohammad Ataei <mammadataei@gmail.com>
2022-09-22 08:50:10 +0330

    feat: add default build configuration

--- build.config.ts
+++ build.config.ts
@@ -4,4 +4,10 @@ export default defineBuildConfig({
   replace: {
     'import.meta.vitest': 'undefined',
   },
+  entries: ['src/index'],
+  declaration: true,
+  clean: true,
+  rollup: {
+    emitCJS: true,
+  },
 })
