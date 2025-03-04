f971dc40c7a90cee427314a013bf2fe696675f84
Mohammad Ataei <mammadataei@gmail.com>
2025-03-04 16:04:32 +0330

    fix: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,19 @@
-import js from '@eslint/js'
+// @ts-check
+
+import eslint from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-config-prettier'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      prettier,
-    ],
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    rules: {
-      //
-    },
+export default tseslint.config({
+  ignores: ['dist'],
+  extends: [eslint.configs.recommended, tseslint.configs.recommended, prettier],
+  files: ['**/*.{ts,tsx,js,jsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
   },
-)
+  rules: {
+    //
+  },
+})
